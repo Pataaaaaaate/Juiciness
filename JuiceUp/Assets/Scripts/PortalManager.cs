cfa9a113adce16e1b889d9d6035e7ceda2de524d
Meline Monteiro <meline.monteiro04@gmail.com>
2025-04-28 17:35:09 +0200

    portail/feuille

--- JuiceUp/Assets/Scripts/PortalManager.cs
+++ JuiceUp/Assets/Scripts/PortalManager.cs
@@ -9,23 +9,24 @@ public class PortalManager : MonoBehaviour
     public float EnemyPeriod;
     public GameObject ennemiApparition;
     private float timeApparition = 1f;
+
+    public SpriteRenderer portal;
+
     void Start()
     {
-        
         InvokeRepeating("InvokeEnemy", 0, EnemyPeriod);
     }
 
     void InvokeEnemy()
     {
-
-        
         Instantiate(EnemyPrefab, transform.position, transform.rotation);
         Instantiate(ennemiApparition, transform.position, transform.rotation);
+    }
 
-
-        
+    private void Update()
+    {
+        portal.transform.rotation = Quaternion.Euler(0, 0, -10 * Time.time);
     }
 
 
-   
 }
